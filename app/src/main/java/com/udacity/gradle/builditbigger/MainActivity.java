d04e14fe883f7ccbc69521e42acb7976a0caf258
Amrhal <eng.amrhalawani@rocketmail.com>
2018-07-27 22:08:48 +0200

    android Lib created

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.amrhal.jokeandroidlib.MainActivityAndroidLib;
 import com.amrhal.jokesjlib.Jokes;
 
 
@@ -45,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     public void tellJoke(View view) {
         Jokes newJoke = new Jokes();
         String jokeFromJokesJavaLib = newJoke.getRandomOne();
+        Intent i = new Intent(this,MainActivityAndroidLib.class);
+        i.putExtra("joke",jokeFromJokesJavaLib);
+        startActivity(i);
         Toast.makeText(this, jokeFromJokesJavaLib , Toast.LENGTH_SHORT).show();
     }
 
